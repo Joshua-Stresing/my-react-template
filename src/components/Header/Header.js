e266a8d4eaf3f6314236889b27d843a7eae23a84
Joshua Stresing <pyrotorch69@yahoo.com>
2022-03-09 13:15:41 -0800

    trying to save my stuff

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Header.css';
+
+// Set up a header
+export default function Header() {
+  return <header>Name of Project</header>;
+}
---
+++ src/components/Object/Object.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//example
+export default function Object({ name, says, top, left, type }) {
+  return ( 
+    <div>
+    </div>);
+}
